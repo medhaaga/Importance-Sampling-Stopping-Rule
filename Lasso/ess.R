6c2cb4f7e66e200659856c71d70da9ed0525100f
Medha Agarwal <medhaaga@gmail.com>
2022-06-28 17:39:22 -0700

    ess plots

--- Lasso/ess.R
+++ Lasso/ess.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 library(scales)
 library(ggpubr)
-
+library(ISLR)
 
 load("Out/lasso_is.Rdata")
 
@@ -9,10 +9,11 @@ load("Out/lasso_is.Rdata")
 
 data(Hitters)
 Hitters <- na.omit(Hitters)
-N <- nrow(is_samp)
 
 is_samp <- is_mod$eta
 is_weights <- is_mod$weight
+N <- length(is_weights)
+
 n_samp <- seq(1e3, N, (N/10))
 mESS <- rep(0, length(n_samp))
 ESS <- matrix(0, nrow = 5, ncol = length(n_samp))
@@ -32,11 +33,11 @@ for (i in (1:length(n_samp))){
 
 pdf(file = "Out/is_mESS.pdf", height = 5, width  = 5)
 plot(n_samp, mESS, type = "l", lwd = 2, xlab = "n", main = "IS-INLA ESS vs n", ylab = "Effective Sample Size", col = "blue", ylim = range(mESS, ESS, kESS))
-lines(n_samp, ESS[1,])
-lines(n_samp, ESS[2,])
-lines(n_samp, ESS[3,])
-lines(n_samp, ESS[4,])
-lines(n_samp, ESS[5,])
+lines(n_samp, ESS[1,], lty=2)
+lines(n_samp, ESS[2,], lty=2)
+lines(n_samp, ESS[3,], lty=2)
+lines(n_samp, ESS[4,], lty=2)
+lines(n_samp, ESS[5,], lty=2)
 lines(n_samp, kESS, col = "red")
-legend("topright", legend = c("m-ESS", "ESS", "k-ESS"), lwd = c(2,1,1), col = c("blue", "black", "red"), cex = 0.8)
+legend("topleft", legend = c("m-ESS", "ESS", "k-ESS"), lty = c(1,2,1), lwd = c(2,1,1), col = c("blue", "black", "red"), cex = 0.8)
 dev.off()
