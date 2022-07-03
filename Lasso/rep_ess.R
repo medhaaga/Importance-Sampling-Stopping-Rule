6c10ad2c07001d4f01c4fe619e026f5657a742ec
Dootika Vats <dootika1990@gmail.com>
2022-07-03 22:42:29 +0530

    Pushing final lasso results

--- Lasso/rep_ess.R
+++ Lasso/rep_ess.R
@@ -3,7 +3,7 @@ load("Out/lasso_reps.Rdata")
 
 d <- dim(all_out[[1]]$eta)[2]
 N <- length(all_out[[1]]$weight)
-reps <- 1
+reps <- 10
 
 
 n_samp <- seq(1e3, 1e4, (N/10))
@@ -47,22 +47,23 @@ mean_oESS <- apply(oESS, c(1,2), mean)
 mean_ESS <- apply(ESS, c(1,2), mean)
 se_kESS <- 2*apply(kESS, 1, sd)/sqrt(reps)
 se_mESS <- 2*apply(mESS, 1, sd)/sqrt(reps)
-se_ESS1 <- 2*apply(ESS[,1,], 1, sd)/sqrt(reps)
-se_ESS2 <- 2*apply(ESS[,2,], 1, sd)/sqrt(reps)
-se_ESS3 <- 2*apply(ESS[,3,], 1, sd)/sqrt(reps)
-se_ESS4 <- 2*apply(ESS[,4,], 1, sd)/sqrt(reps)
-se_ESS5 <- 2*apply(ESS[,5,], 1, sd)/sqrt(reps)
+#se_ESS1 <- 2*apply(ESS[,1,], 1, sd)/sqrt(reps)
+#se_ESS2 <- 2*apply(ESS[,2,], 1, sd)/sqrt(reps)
+#se_ESS3 <- 2*apply(ESS[,3,], 1, sd)/sqrt(reps)
+#se_ESS4 <- 2*apply(ESS[,4,], 1, sd)/sqrt(reps)
+#se_ESS5 <- 2*apply(ESS[,5,], 1, sd)/sqrt(reps)
 
 ### Plot
 
 pdf(file = "Out/reps_ESS.pdf", height = 5, width  = 5)
-plot(n_samp, mean_mESS, type = "l", lwd = 2, xlab = "n", ylab = "ESS/n", col = "blue", ylim = range(mESS, oESS, kESS))
+plot(n_samp, mean_mESS, type = "l", lwd = 2, xlab = "Sample Size", ylab = "ESS/n", col = "blue", ylim = c(0,.31))
 segments(x0 = n_samp, y0 = mean_mESS - se_mESS, y1 = mean_mESS + se_mESS, col = adjustcolor("blue", alpha = 0.5))
 for(i in 1:d)
 {
-  lines(n_samp, mean_oESS[,i], lty=2)
+  lines(n_samp, mean_oESS[,i], lty=2, col = adjustcolor("black", alpha = 0.7))
 }
 lines(n_samp, mean_kESS, col = "red")
 segments(x0 = n_samp, y0 = mean_kESS - se_kESS, y1 = mean_kESS + se_kESS, col = adjustcolor("red", alpha = 0.5))
-legend("bottomright", legend = c("m-ESS", "ESS", "Kong"), lty = c(1,2,1), lwd = c(2,1,1), col = c("blue", "black", "red"), cex = 0.8)
+legend("topright", legend = c("m-ESS", "Owen", "Kong"), lty = c(1,2,1), lwd = c(2,1,1), col = c("blue", "black", "red"), cex = 0.8)
 dev.off()
+
